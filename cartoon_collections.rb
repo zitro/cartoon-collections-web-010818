08080a8317b615ad192ec9cf2b011f994cd7a26c
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 17:39:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each? {|n| puts n.length.to_i >= 3 }
+  array.each {|n| puts n.length.to_i >= 3 }
 end
 
 def find_the_cheese# code an argument here
