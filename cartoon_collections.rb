35897152a36dcbf5c761d0fa8d1af4dc4cc1fd88
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 18:08:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,10 +5,9 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map! {|name| "#{name.capitalize}!" }
+  array.map {|i| "#{i.capitalize}!" }
 end
 
-=begin
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each do |i|
@@ -18,14 +17,13 @@ def long_planeteer_calls(array)# code an argument here
     puts false
   end
 end
-=end
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include? cheese_types
-    return true
-  else
-    return false
-  end
+cheese = array & cheese_types
+if cheese.length >= 1
+  puts cheese[0]
+else
+  puts false
 end
