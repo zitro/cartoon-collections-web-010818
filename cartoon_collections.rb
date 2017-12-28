90dbcfd350ef03070e62a375f4128f035860183d
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 18:26:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,9 +23,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese = %w(gouda cheddar camembert)
-  if array.in?(cheese_types)
-    true
-  else
-    false
+  array & cheese_types
 
 end
