d378f2eea3e99ce39472559717ef1dddda91ed47
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 19:10:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,17 +7,11 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map {|i| "#{i.capitalize}!" }
 end
-=begin
+
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each do |i|
-  if i >= 5
-    puts true
-  else
-    puts false
-  end
+array.split.any? { |x| x.length > 4 }
 end
-=end
 
 =begin
 def find_the_cheese(array)# code an argument here
