93143fa76bcde8dc925d722c575d5c77a75481d5
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 18:41:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,8 @@ def find_the_cheese(array)# code an argument here
 
   cheese = array & cheese_types
   finalCheese = cheese.join
-  if cheese
-    finalCheese
+  if cheese == false
+    nil
   else
     finalCheese
   end
