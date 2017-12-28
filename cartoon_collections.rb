a75ebabd8b671fb7b8a5029966e752f6cdc052cb
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 18:09:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
   array.map {|i| "#{i.capitalize}!" }
 end
-
+=begin
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each do |i|
@@ -17,6 +17,7 @@ def long_planeteer_calls(array)# code an argument here
     puts false
   end
 end
+=end
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
