56fe7fa91fe06339a471e0c820ac48238dacfe18
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 19:16:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)# code an argument here
 array.any? { |i| i.length > 4 }
 end
 
-=begin
+
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
@@ -27,4 +27,3 @@ def find_the_cheese(array)# code an argument here
   end
 
 end
-=end
