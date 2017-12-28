a177b8d276fc46a89d9f76f829f6b8ab0d2f0fa3
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-28 17:54:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include(array)
 end
